54d0bda2ca7bef4223a935a200d29033653f4be4
Rose Barcus <angela.rose.barcus@gmail.com>
2021-03-30 13:56:39 -0500

    completed arrays exercise

--- js/planets-array.js
+++ js/planets-array.js
@@ -17,37 +17,41 @@
      * Read each console log below, and write some javascript code to perform
      * the step that it describes
      */
-planets.unshift("The Sun");
+
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
+    //this modifies the array.
+    planets.unshift("The Sun");
+    // this returns the length of the modified array,
+    console.log("What does this log?". planets.unshift("The Sun"));
     console.log(planets);
 
-planets.push("Pluto");
+
 
     console.log('Adding "Pluto" to the end of the planets array.');
+    planets.push("Pluto");
+    //console.log(planets.push("Pluto")); ** if we wanted the length of the modified array.
     console.log(planets);
-
-planets.shift("The Sun");
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
+    planets.shift();
     console.log(planets);
 
-planets.pop("Pluto");
 
     console.log('Removing "Pluto" from the end of the planets array.');
+    planets.pop();
     console.log(planets);
-
-console.log(planets.indexOf("Earth"));
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
-
+    console.log(planets.indexOf("Earth"));
     console.log(planets);
-    planets.reverse();
+
+
     console.log("Reversing the order of the planets array.");
+    planets.reverse();
     console.log(planets);
-
-    planets.sort();
 
     console.log("Sorting the planets array.");
+    planets.sort();
     console.log(planets);
 })();
