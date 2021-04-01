197b449be2a7ec4b17097c62f45b78b9fc072f9c
Rose Barcus <angela.rose.barcus@gmail.com>
2021-04-01 11:15:06 -0500

    js break and continue exercise and objects update

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -2,15 +2,22 @@
 
 var oddNumber = 0
 
-while (oddNumber !== "You're never gonna come up with this?") {
+/*while (oddNumber !== "You're never gonna come up with this?") {
 
 }
 while (oddNumber % 2 !== 1 || (oddNumber > 0 && oddNumber <50)) {
     oddNumber = parseFloat(prompt("Please give me an odd number between 1 and 50."));
 
+}*/
+
+var oddNumber = parseInt(prompt("Give me an odd number between 1 and 50, please?"));
+
+while(true){
+    if (oddNumber % 2 !== 1 || (oddNumber > 0 && oddNumber <50)) {
+
+    }
+    oddNumber = parseInt(prompt("Give me an odd number between 1 and 50, please?"));
 }
-
-
 for (var i =1; i <= 50; i = 1 + 2){
     if (i === oddNumber) {
         console.log("Yikes, skipping number: " + oddNumber);
