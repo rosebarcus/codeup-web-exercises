bccef9e5e5cedbbc7145af53230f5d9b8e68e966
Rose Barcus <angela.rose.barcus@gmail.com>
2021-05-02 21:11:43 -0500

    adding to function drills

--- js/extra-function-drills.js
+++ js/extra-function-drills.js
@@ -2,7 +2,7 @@
 
 console.log("Hey, Rose. Get some keyboard time in!");
 
-var number = 7;
+var number = 20;
 function isOdd(number) {
     if (number % 2 === 0) {
         return ("The number is even");
@@ -12,3 +12,24 @@ function isOdd(number) {
 }
 
 console.log(isOdd(number));
+
+function isEven (number) {
+    if (number % 2 ===0){
+        return ("The number is even");
+    } else {
+        return "the number is odd"
+    }
+}
+
+console.log(isEven(number));
+
+var input = prompt("Please enter a message");
+
+function identity(input) {
+    if(input === input) {
+        return alert("you entered " + input);
+    }
+}
+
+
+
