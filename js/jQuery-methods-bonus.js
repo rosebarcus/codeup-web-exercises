8a7ec990427281d30c4f4a217d0a932f42516c2a
Rose Barcus <angela.rose.barcus@gmail.com>
2021-05-05 10:43:32 -0500

    working on bonus traversing exercise

--- js/jQuery-methods-bonus.js
+++ js/jQuery-methods-bonus.js
@@ -1,22 +1,37 @@
-"use strict";
-
 $(document).ready(function(){
+
     $('button').click(function(){
         var nextImage = $(this).parent().next().children().first();
         var nextImageSrc = nextImage.attr('src');
         var thisSrc = $(this).prev().attr('src');
-        $(this).prev().attr('src', nextImage);
+        $(this).prev().attr('src', nextImageSrc);
         nextImage.attr('src', thisSrc);
 
     });
-    // missing the middle button here
+
+    $('#button-2').click(function(){
+        var randomNumber = Math.floor(Math.random()*2);
+        var nextImage = $(this).parent().next().children().first();
+        var nextImageSrc = nextImage.attr('src');
+        var previousImage = $(this).parent().prev().children().first();
+        var previousImageSrc = previousImage.attr('src');
+        var thisImage = $(this).prev();
+        var thisSrc = thisImage.attr('src');
+        if (randomNumber === 0){
+            previousImage.attr('src', thisSrc);
+            thisImage.attr('src', previousImageSrc);
+        } else {
+            $(this).prev().attr('src', nextImageSrc);
+            nextImage.attr('src', thisSrc);
+        }
+    });
 
     $('#button-3').click(function(){
         var previousImage = $(this).parent().prev().children().first();
         var previousImageSrc = previousImage.attr('src');
         var thisImage = $(this).prev();
-        var thisSrc = thisImage.attr('src', thisSrc);
+        var thisSrc = thisImage.attr('src');
         previousImage.attr('src', thisSrc);
-        thisImage.attr('src', previousImage);
+        thisImage.attr('src', previousImageSrc);
     });
 });
