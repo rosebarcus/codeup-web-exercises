75be38845d9ade1673c960a1255adb2630f45726
Rose Barcus <angela.rose.barcus@gmail.com>
2021-05-17 16:57:03 -0500

    creating function for github api request for users last commit

--- js/promises.js
+++ js/promises.js
@@ -1,2 +1,11 @@
 "use strict";
 
+function githubUserLastCommit (username){
+    fetch (${https://api.github.com/users/tater/events}, {
+        headers:
+            {'Authorization': GITHUB_ACCESS_TOKEN}})
+
+}
+
+console.log(githubUserLastCommit());
+
