6cf9e2641c5d187fe9136537d400d108bee9f2fe
Rose Barcus <angela.rose.barcus@gmail.com>
2021-05-17 13:58:12 -0500

    finished map, filter, reduce exercises

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -52,3 +52,56 @@ let trilinguals = users.filter(function (user){
     return user.languages.length >= 3;
 });
 
+const yearsOfExperience = users.reduce(function (accumulator, experience) {
+    accumulator.push(experience.yearsOfExperience);
+    return accumulator;
+}, []);
+
+console.log(yearsOfExperience);
+
+let totalYearsOfExperience = users.reduce(function (total, user){
+    let yearsOfExperience = user.yearsOfExperience;
+    return total + yearsOfExperience;
+}, 0);
+
+console.log(totalYearsOfExperience);
+
+let emailsViaReduce = users.reduce(function (accumulator, user){
+    accumulator.push(user.email);
+    return accumulator;
+}, []);
+
+let longestEmail = users.reduce(function (accumulator, user, index, userArray){
+    if (index === userArray.length -1){
+        accumulator.push(user.email);
+        accumulator.sort(function (email1, email2){
+            return email2.length - email1.length
+        });
+        return accumulator[0];
+    } else {
+        accumulator.push(user.email);
+        return accumulator;
+    }
+
+},[]);
+
+console.log(longestEmail);
+
+let names = users.reduce(function (accumulator, user){
+    accumulator.push(user.name);
+    return accumulator;
+}, []);
+
+let namesNicelyJoined = users.reduce(function (accumulator, user, index, usersArray){
+    if(index === usersArray.length -1) {
+        accumulator.push(user.name);
+        let niceJoin = accumulator.join(', ');
+        niceJoin = "Your instructors are: " + niceJoin + ".";
+        return niceJoin;
+    } else {
+        accumulator.push(user.name);
+        return accumulator;
+    }
+}, []);
+
+console.log(namesNicelyJoined);
