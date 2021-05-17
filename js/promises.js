3a4ca4460b85782fb29310ad8814c968fb74c6b9
Rose Barcus <angela.rose.barcus@gmail.com>
2021-05-17 17:41:25 -0500

    testing promise exercise functionality

--- js/promises.js
+++ js/promises.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const gitIt = fetch ('https://api.github.com/users/rosebarcus/events', {
+const gitIt = fetch (`https://api.github.com/users/rosebarcus/events`, {
         headers:
             {'Authorization': GITHUB_ACCESS_TOKEN}
 
@@ -11,4 +11,28 @@ const gitIt = fetch ('https://api.github.com/users/rosebarcus/events', {
     console.log(data);
 })
 
+console.log(getGithubUsernames(gitIt));
 
+function getGithubUsernames(username) {
+    return fetch(`https://api.github.com/users/${username}/events`, {
+        headers: {'Authorization': GITHUB_ACCESS_TOKEN}
+    }).then(response => response.json().then(data => {
+        console.log(data);
+        var latestPush = data[0].created_at
+        let date = new Date(latestPush);
+
+        console.log(date.toString());
+    }))
+}
+getGithubUsernames('rosebarcus');
+
+
+
+//TODO finish creating your own promise:
+/*
+const myPromise = new Promise ((happy, sad) => {
+    function wait (ms) {
+
+    }
+
+})*/
